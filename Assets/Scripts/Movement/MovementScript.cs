af68bf320666d7643ccbf783bd13e65ae3524381
ksarica <srca.kaan@gmail.com>
2021-02-23 15:22:09 +0300

    Practices for Unity

---
+++ Assets/Scripts/Movement/MovementScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Movement
+{
+    public class MovementScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        //[SerializeField] private float _speed;
+        [SerializeField] private MovementSettings _movementSettings;
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position += Vector3.forward * _movementSettings.Speed * Time.deltaTime;
+            transform.Rotate(Vector3.up, 5 * _movementSettings.Speed * Time.deltaTime);
+            //transform.Rotate(Vector3.forward * _speed * Time.deltaTime);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Movement/MovementSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Movement
+{
+    [CreateAssetMenu(menuName = "Movement/MovementSettings/Setting1")]
+    public class MovementSettings : ScriptableObject
+    {
+        [SerializeField] private float _speed;
+        public float Speed { get { return _speed; } }
+    }
+}
